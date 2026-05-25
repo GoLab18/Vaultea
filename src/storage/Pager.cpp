6e356ae8a3b7ed727dbdee6e72c24d21257f1067
GoLab18 <kuba1golab@gmail.com>
2026-05-25 22:12:58 +0200

    Pager new checks and fixes

--- src/storage/Pager.cpp
+++ src/storage/Pager.cpp
@@ -1,5 +1,8 @@
 #include "Pager.h"
 
+#include <algorithm>
+#include <stdexcept>
+
 Pager::Pager(StorageEngine &storage, uint32_t pageSize, size_t maxPages)
     : storage(storage), pageSize(pageSize), maxPages(maxPages) {
   nextPageId = storage.fileSize() / pageSize;
@@ -9,18 +12,15 @@ Page &Pager::getPage(PageId id) {
   auto it = cache.find(id);
 
   if (it == cache.end()) {
-
     if (cache.size() >= maxPages) {
       evictOne();
     }
 
     loadPage(id);
-
     it = cache.find(id);
   }
 
   it->second.pinCount++;
-
   touch(id);
 
   return it->second;
@@ -28,10 +28,8 @@ Page &Pager::getPage(PageId id) {
 
 void Pager::unpin(PageId id) {
   auto it = cache.find(id);
-
-  if (it == cache.end()) {
+  if (it == cache.end())
     return;
-  }
 
   if (it->second.pinCount > 0) {
     it->second.pinCount--;
@@ -40,9 +38,8 @@ void Pager::unpin(PageId id) {
 
 void Pager::markDirty(PageId id) {
   auto it = cache.find(id);
-
   if (it == cache.end()) {
-    throw std::runtime_error("Page not cached");
+    throw std::runtime_error("markDirty: page not cached");
   }
 
   it->second.dirty = true;
@@ -53,27 +50,29 @@ void Pager::touch(PageId id) {
 
   if (it != lruMap.end()) {
     lru.erase(it->second);
+    lruMap.erase(it);
   }
 
   lru.push_front(id);
-
   lruMap[id] = lru.begin();
 }
 
 void Pager::evictOne() {
+  while (!lru.empty()) {
+    PageId victimId = lru.back();
+    lru.pop_back();
 
-  for (auto it = lru.rbegin(); it != lru.rend(); ++it) {
-    PageId victimId = *it;
-
-    auto pageIt = cache.find(victimId);
-
-    if (pageIt == cache.end()) {
+    auto it = cache.find(victimId);
+    if (it == cache.end()) {
+      lruMap.erase(victimId);
       continue;
     }
 
-    Page &page = pageIt->second;
+    Page &page = it->second;
 
     if (page.pinCount > 0) {
+      lru.push_front(victimId);
+      lruMap[victimId] = lru.begin();
       continue;
     }
 
@@ -81,34 +80,34 @@ void Pager::evictOne() {
       writePageToDisk(page);
     }
 
-    lru.erase(lruMap[victimId]);
-
+    cache.erase(it);
     lruMap.erase(victimId);
-
-    cache.erase(victimId);
-
     return;
   }
 
-  throw std::runtime_error("No evictable pages available");
+  throw std::runtime_error("evictOne: no evictable pages");
 }
 
 void Pager::loadPage(PageId id) {
-
-  auto raw = storage.read(pageOffset(id), pageSize);
-
   Page page;
-
   page.id = id;
+  page.data.resize(pageSize, 0);
+  page.dirty = false;
+  page.pinCount = 0;
 
-  page.data = std::move(raw);
+  uint64_t offset = pageOffset(id);
+  uint64_t fileSize = storage.fileSize();
+
+  if (offset < fileSize) {
+    auto raw = storage.read(offset, pageSize);
+    page.data = std::move(raw);
+  }
 
   cache[id] = std::move(page);
 }
 
 void Pager::writePageToDisk(Page &page) {
   storage.write(pageOffset(page.id), page.data.data(), pageSize);
-
   page.dirty = false;
 }
 
@@ -126,16 +125,21 @@ void Pager::freePage(PageId id) {
   auto it = cache.find(id);
 
   if (it != cache.end()) {
-
     if (it->second.pinCount > 0) {
-      throw std::runtime_error("Cannot free pinned page");
+      throw std::runtime_error("freePage: page is pinned");
     }
 
-    lru.erase(lruMap[id]);
-
-    lruMap.erase(id);
+    if (it->second.dirty) {
+      writePageToDisk(it->second);
+    }
 
     cache.erase(it);
+  }
+
+  auto lm = lruMap.find(id);
+  if (lm != lruMap.end()) {
+    lru.erase(lm->second);
+    lruMap.erase(lm);
   }
 
   freePages.push_back(id);
--- src/storage/Pager.h
+++ src/storage/Pager.h
@@ -4,6 +4,7 @@
 #include "Page.h"
 #include "StorageEngine.h"
 
+#include <cstdint>
 #include <list>
 #include <unordered_map>
 #include <vector>
@@ -17,25 +18,21 @@ public:
   Page &getPage(PageId id);
 
   void unpin(PageId id);
-
   void markDirty(PageId id);
 
   PageId allocatePage();
-
   void freePage(PageId id);
 
   void flush();
 
 private:
   StorageEngine &storage;
-
   uint32_t pageSize;
   size_t maxPages;
 
   std::unordered_map<PageId, Page> cache;
 
   std::list<PageId> lru;
-
   std::unordered_map<PageId, std::list<PageId>::iterator> lruMap;
 
   std::vector<PageId> freePages;
@@ -44,14 +41,11 @@ private:
 
 private:
   inline uint64_t pageOffset(PageId id) const {
-    return id * static_cast<uint64_t>(pageSize);
+    return static_cast<uint64_t>(id) * pageSize;
   }
 
   void touch(PageId id);
-
   void evictOne();
-
   void loadPage(PageId id);
-
   void writePageToDisk(Page &page);
 };
