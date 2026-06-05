d99dda1cf2449752dcae99e2d33d168f31a8b9c1
GoLab18 <kuba1golab@gmail.com>
2026-06-05 12:50:24 +0200

    Added INVALID_PAGE state constexpr

--- src/storage/page/Constants.h
+++ src/storage/page/Constants.h
@@ -2,15 +2,18 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <limits>
 
 namespace vault::storage::page {
 
 inline constexpr uint32_t DEFAULT_PAGE_SIZE = 4096;
 
-inline constexpr std::size_t HEADER_SIZE = 8;
+inline constexpr std::size_t HEADER_SIZE = 23;
 inline constexpr std::size_t SLOT_SIZE = 5;
 
 using PageId = uint64_t;
 using SlotId = uint16_t;
+
+inline constexpr PageId INVALID_PAGE = std::numeric_limits<PageId>::max();
 
 } // namespace vault::storage::page
